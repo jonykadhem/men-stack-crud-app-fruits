63add447977bc5771f57d5be343b79987d44eeb1
unknown <jonykadhem@gmail.com>
2026-07-09 12:14:25 +0300

    creating the model

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const dns = require("node:dns");
 
-const express = require('express');
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+const dotenv = require('dotenv').config()
+const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-
-const app = express();
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected',()=>{
+    console.log(`connected to mongoDB ${mongoose.connection.name} 🥭`)
+})
+const app = express()
 app.use(morgan('div'))
 
 app.get('/', async(req, res) =>{
