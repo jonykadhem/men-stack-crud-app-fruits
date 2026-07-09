eff43cc0e8ecd921d40221b31e265bb94e96d43a
unknown <jonykadhem@gmail.com>
2026-07-09 12:44:37 +0300

    creating an object

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const Fruit = require('./models/fruit.js')
 
 mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected',()=>{
@@ -17,6 +18,20 @@ app.get('/', async(req, res) =>{
     res.render('home.ejs')
 })
 
+app.get('/fruits', async(req,res)=> {
+    
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
+
+//code graveyard ==================================>
+
+    // const fruitData = {}
+    // fruitData.name = 'peach'
+    // fruitData.isReadyToEat = false
+
+    // let createdFruit = await Fruit.create(fruitData)
+    // res.send(createdFruit)
